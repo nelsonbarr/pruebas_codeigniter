aab05489911aa900c7e044ca582ddf6fb0ec4fa2
nelsonbarr <nelsonbarr@gmail.com>
2018-09-24 23:57:53 -0500

    CAMBIOS EN LOGIN Y PRINCIPIO DE DASHBOARD

--- application/views/plantillas/back_end/header.php
+++ application/views/plantillas/back_end/header.php
@@ -8,14 +8,14 @@
         <title>CONTROL DE CITAS</title>
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
         <meta name="viewport" content="width=device-width" />
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-        <link href="assets/css/material-dashboard.css" rel="stylesheet"/>
-        <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-        <script src="assets/js/jquery.min.js" type="text/javascript"></script>
-        <link href='assets/fullcalendar/fullcalendar.min.css' rel='stylesheet' />
-        <link href='assets/fullcalendar/fullcalendar.print.css' rel='stylesheet' media='print' />
-        <script src='assets/fullcalendar/moment.min.js'></script>
-        <script src='assets/fullcalendar/fullcalendar.min.js'></script>
+        <link href="<?php echo base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="<?php echo base_url() ?>assets/css/material-dashboard.css" rel="stylesheet"/>
+        <link href="<?php echo base_url() ?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <script src="<?php echo base_url() ?>assets/js/jquery.min.js" type="text/javascript"></script>
+        <link href='<?php echo base_url() ?>assets/fullcalendar/fullcalendar.min.css' rel='stylesheet' />
+        <link href='<?php echo base_url() ?>assets/fullcalendar/fullcalendar.print.css' rel='stylesheet' media='print' />
+        <script src='<?php echo base_url() ?>assets/fullcalendar/moment.min.js'></script>
+        <script src='<?php echo base_url() ?>assets/fullcalendar/fullcalendar.min.js'></script>
     </head>
     <body>
         <div id="header">
